ee933abd383d2a3f82881f1542d979f5a3973bf4
Eric de Moraes <eric.guedes.sm@gmail.com>
2020-04-02 22:29:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import { Container } from './styles';
+
+export default function App() {
+    return (
+        <div style={{ width: "100%", height: "100%" }}>
+            <h1>Hello world</h1>
+        </div>
+    );
 }
-
-export default App;
