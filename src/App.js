41dc5e272008bdae41cd1db06dde38cd3bf7dc9f
Eric de Moraes <eric.guedes.sm@gmail.com>
2020-04-04 14:16:03 -0300

    configuring the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-    return (
-        <div style={{ width: "100%", height: "100%" }}>
-            <h1>Hello world</h1>
-        </div>
-    );
+    return <Routes />;
 }
