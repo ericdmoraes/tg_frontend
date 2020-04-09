e4d5fd6ca5a71ee3e15addfd737c76c287be003a
Eric de Moraes <eric.guedes.sm@gmail.com>
2020-04-09 18:21:07 -0300

    removing extension needed

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,12 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 
 export default (reducers, middlewares) => {
-    return createStore(
-        reducers,
-        compose(
-            applyMiddleware(...middlewares),
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+    return createStore(reducers, compose(applyMiddleware(...middlewares)));
 };
